780b56923e3f353eb62017b5190b6de673243c88
NagaLakshmi477 <jaishikareddy997@gmail.com>
2025-08-23 17:42:19 +0530

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,6 +1,6 @@
 
 
-module "name" {
+module "frontend" {
   source = "git::https://github.com/NagaLakshmi477/terraform-aws-security-group.git?ref=main"
   sg_name = var.frontend_sg_name
   sg_description = var.frontend_sg_description
