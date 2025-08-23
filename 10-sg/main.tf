151891d7cfc82f771148a37779c3c357af93802a
NagaLakshmi477 <jaishikareddy997@gmail.com>
2025-08-23 17:34:55 +0530

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,20 +1,11 @@
-module "frontend" {
+
+
+module "name" {
   source = "git::https://github.com/NagaLakshmi477/terraform-aws-security-group.git?ref=main"
-  project = var.project
-  vpc_id = local.vpc_id
-  environment = var.environment
-  sg_tags = var.sg_tags
-  sg_description = var.frontend_sg_description
   sg_name = var.frontend_sg_name
-
-  
-
+  sg_description = var.frontend_sg_description
+  environment = var.environment
+  project = var.project
+  sg_tags = var.sg_tags
+  vpc_id = local.vpc_id
 }
-
-  # project = var.project
-  # sg_name = var.frontend_sg_name
-  # frontend_sg_description = var.frontend_sg_description
-  
-  # # here vpc id comes from vpc check ssm-pars.io
-  # vpc_id = local.vpc_id
-  # environment = var.environment
