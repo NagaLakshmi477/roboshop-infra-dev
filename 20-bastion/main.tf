fda4901cf943212a811c4d48680a2ac946b7d0fd
NagaLakshmi477 <jaishikareddy997@gmail.com>
2025-08-24 05:12:40 +0530

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -1,7 +1,7 @@
-# resource "aws_instance" "bastion" {
-#   ami = local.ami_id
-#   instance_type = "t3.micro"
-#   tags = {
-#     Name = "hello-bastion"
-#   }
-# }
+resource "aws_instance" "bastion" {
+  ami = local.ami_id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "hello-bastion"
+  }
+}
