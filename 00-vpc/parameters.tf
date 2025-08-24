8fad1cd584be11ee959ef9abf33126dd7525178e
NagaLakshmi477 <jaishikareddy997@gmail.com>
2025-08-25 03:50:59 +0530

    terraform

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -3,3 +3,10 @@ resource "aws_ssm_parameter" "vpc_id" {
   type  = "String"
   value = module.vpc.vpc_id
 }
+
+resource "aws_ssm_parameter" "public_sunet_ids" {
+  name  = "/${var.project}/${var.environment}/public_sunet_ids"
+  type  = "StringList"
+  value = module.vpc.public_sunet_ids
+}
+
