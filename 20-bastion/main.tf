03472d05e6a57bd48f15d0c43677d4a433c5a118
NagaLakshmi477 <jaishikareddy997@gmail.com>
2025-08-25 05:24:17 +0530

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "bastion" {
   # now we need to give subnets if we cannot provide subnets then it will ceate in the default vpc and default subnet
   # but we need roboshop vpc and roboshop subnet(public)
   instance_type = "t3.micro"
-  subnet_id = local.public_subnet_id
+  subnet_id = [local.public_subnet_id]
   tags = merge(
     local.common_tags,{
       Name = "${var.project}-${var.environment}-bastion"
